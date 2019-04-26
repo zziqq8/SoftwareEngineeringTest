a06d354f0bdcc82432694fc8f2aea00688988528
g17962yk <g17962yk@s30508.tsuda.ac.jp>
2019-04-26 15:55:21 +0900

    ウィンドウのタイトルを変更

--- src/com/toy/anagrams/ui/About.java
+++ src/com/toy/anagrams/ui/About.java
@@ -80,7 +80,7 @@ public class About extends JDialog {
         copyrightTextArea.setEditable(false);
         copyrightTextArea.setLineWrap(true);
         copyrightTextArea.setRows(8);
-        copyrightTextArea.setText("Anagrams\n\nCopyright (c) 2003  Irritable Enterprises, Inc.");
+        copyrightTextArea.setText("Anagrams\nチームメンバー：今野由麻,柳沢佳那\nチーム名：水\nCopyright (c) 2003  Irritable Enterprises, Inc.");
         copyrightTextArea.setWrapStyleWord(true);
         copyrightTextArea.setBorder(null);
         copyrightTextArea.setFocusable(false);
